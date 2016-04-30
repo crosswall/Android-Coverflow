583fd0b7e16bfec5ff0c090303d5dfa7cd4b061e
crosswall <vvccrosswall@gmail.com>
2016-04-30 14:49:33 +0800

    optimized code

--- coverflow/src/main/java/me/crosswall/lib/coverflow/CoverFlow.java
+++ coverflow/src/main/java/me/crosswall/lib/coverflow/CoverFlow.java
@@ -3,6 +3,8 @@ package me.crosswall.lib.coverflow;
 import android.support.v4.view.ViewPager;
 
 import me.crosswall.lib.coverflow.core.CoverTransformer;
+import me.crosswall.lib.coverflow.core.syncpager.SyncCoverTransformer;
+import me.crosswall.lib.coverflow.core.syncpager.SyncViewPgaer;
 
 /**
  * Created by yuweichen on 16/4/29.
@@ -15,7 +17,8 @@ public class CoverFlow {
 
  //   public static final float DEFAULT_SPACE_SIZE   = 0f;
 
-    private final ViewPager syncViewPager;
+    private final ViewPager viewPager;
+    private final SyncViewPgaer syncViewPgaer;
     private final float scaleValue;
     private final float pagerMargin;
     private final float spaceSize;
@@ -26,30 +29,39 @@ public class CoverFlow {
             throw new IllegalArgumentException("A non-null CoverFlow.Builde must be provided");
         }
 
-        this.syncViewPager = builder.syncViewPager;
+        this.viewPager = builder.viewPager;
+        this.syncViewPgaer = builder.syncViewPgaer;
         this.scaleValue  = builder.scaleValue;
         this.pagerMargin = builder.pagerMargin;
         this.spaceSize   = builder.spaceSize;
 
-        if(null == this.syncViewPager){
-            throw new IllegalArgumentException("A non-null ViewPager must be provided");
+        if(this.viewPager != null){
+            this.viewPager.setPageTransformer(false,
+                    new CoverTransformer(this.scaleValue,this.pagerMargin,this.spaceSize));
+        }else if(this.syncViewPgaer !=null){
+            this.syncViewPgaer.setPageTransformer(false,new SyncCoverTransformer(this.scaleValue,this.pagerMargin,this.spaceSize));
         }
 
-        this.syncViewPager.setPageTransformer(false,
-                new CoverTransformer(this.scaleValue,this.pagerMargin,this.spaceSize));
 
     }
 
     public static class Builder {
-        private ViewPager syncViewPager;
+        private ViewPager viewPager;
+        private SyncViewPgaer syncViewPgaer;
         private float scaleValue;
         private float pagerMargin;
         private float spaceSize;
 
-        public CoverFlow.Builder with(ViewPager syncViewPager) {
-            this.syncViewPager = syncViewPager;
+        public CoverFlow.Builder with(ViewPager viewPager) {
+            this.viewPager = viewPager;
             return this;
         }
+
+        public CoverFlow.Builder withSync(SyncViewPgaer syncViewPgaer){
+            this.syncViewPgaer = syncViewPgaer;
+            return this;
+        }
+
 
         public CoverFlow.Builder scale(float scaleValue) {
             this.scaleValue = scaleValue;
